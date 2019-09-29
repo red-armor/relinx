c21a5914ed8d4d0b671ae8e472ca13ad1078e6a5
youchao liu <ryuforever@163.com>
2019-09-29 23:10:09 +0800

    rm console

--- src/Provider.js
+++ src/Provider.js
@@ -35,7 +35,6 @@ export default ({ store, children }) => {
         central.reconcileWithPaths([storeKey, key], changedValue[key])
       })
     })
-    console.log('central : ', central)
   }, [value])
 
   // Context only need to pass `dispatch`, state value could get from isolate `useTracker` instance
