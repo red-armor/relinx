bfbb54f9bcc729227eb22347c292ecda421950e1
youchaoliu <youchaoliu@gmail.com>
2019-09-30 15:06:31 +0800

    `unsubscribe` on `unmount`

--- src/hooks/useRelinx.js
+++ src/hooks/useRelinx.js
@@ -1,20 +1,29 @@
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState, useEffect, useRef } from 'react'
 import context from '../context'
 import useTracker from '../tracker/useTracker'
 
 export default name => {
   const { dispatch } = useContext(context)
   const [value, setValue] = useState(0)
+  const state = useRef()
+  const unsub = useRef(() => {})
 
   // Every `useTracker` will has an isolated state manager; It has two functionality:
   // 1. register reactive `path`
   // 2. reactive to central data change and propagate change to connected context
-  const [state, unsubscribe] = useTracker(() => {
+  const tracker = useTracker(() => {
     setValue(value + 1)
   }, name)
 
-  // When it comes to `unmount`, computation's listener should be clear.
-  useEffect(() => unsubscribe, [])
+  state.current = tracker[0]
+  unsub.current = tracker[1]
 
-  return [state, dispatch]
+  // When it comes to `unmount`, computation's listener should be clear.
+  // Note that use `unsub.current` to get the latest created computation
+  useEffect(() => () => unsub.current(), [])
+
+  // The following style is in-corrent; `unsub.current` is the original closure callback;
+  // useEffect(() => unsub.current, [])
+
+  return [state.current, dispatch]
 }
