f406e274b07ef5668872fe6c656d0fc61c9765b4
youchao liu <youchaoliu@xiaohongshu.com>
2020-09-27 13:51:25 +0800

    fix: diff strategies

--- src/Application.ts
+++ src/Application.ts
@@ -1,8 +1,9 @@
 import PathNode from './PathNode';
-import is from './utils/is';
+// import is from './utils/is';
 import infoLog from './utils/infoLog';
-import { isObject, isMutable, isTypeEqual } from './utils/ifType';
-import diffArraySimple from './utils/diffArraySimple';
+import { isTypeEqual, isNumber, isString, isMutable } from './utils/ifType';
+// import diffArraySimple from './utils/diffArraySimple';
+import shallowEqual from './utils/shallowEqual';
 import {
   IApplication,
   GenericState,
@@ -115,11 +116,13 @@ class Application<T, K extends keyof T> implements IApplication<T, K> {
   treeShake({ storeKey, changedValue }: { storeKey: K; changedValue: object }) {
     const branch = this.node.children[storeKey as any];
     const baseValue = this.base[storeKey];
-    const rootBaseValue = baseValue;
+    // const rootBaseValue = baseValue;
     const nextValue = { ...baseValue, ...changedValue };
 
     // why it could be undefined. please refer to https://github.com/ryuever/relinx/issues/4
     if (!branch) return;
+
+    const patchers: any = [];
 
     const toDestroy: Array<Function> = [];
     const compare = (
@@ -133,112 +136,155 @@ class Application<T, K extends keyof T> implements IApplication<T, K> {
       collections: Array<string>,
       operation: Array<Operation>
     ) => {
-      if (is(baseValue, nextValue)) return;
+      const keysToCompare2 = Object.keys(branch.children);
 
-      // TODO, add description...only primitive type react...
-      if (!isTypeEqual(baseValue, nextValue) || !isMutable(nextValue)) {
-        if (branch.patchers.length) {
-          branch.patchers.forEach(patcher => {
-            this.pendingPatchers.push({ collections, patcher, operation });
-          });
-          // delete should be placed after collection...
-          // `branch.patchers` will be modified on `markDirty`..
-          // branch.patchers.forEach(patcher => patcher.markDirtyAll())
-          branch.patchers.forEach(patcher => patcher.markDirty());
-        }
-      }
+      keysToCompare2.forEach(key => {
+        const oldValue = baseValue[key];
+        const newValue = nextValue[key];
 
-      const caredKeys = Object.keys(branch.children);
-      let keysToCompare = caredKeys;
-      let keysToDestroy: Array<string> = [];
-      const currentOperation: Array<Operation> = [];
+        if (shallowEqual(oldValue, newValue)) return;
 
-      // 处理，如果说array中的一项被删除了。。。。
-      if (isTypeEqual(baseValue, nextValue) && Array.isArray(nextValue)) {
-        const baseLength = (baseValue as any).length;
-        const nextLength = nextValue.length;
-
-        if (nextLength < baseLength) {
-          keysToCompare = caredKeys.filter(
-            key => parseInt(key, 10) < nextLength || key === 'length'
-          );
-          keysToDestroy = caredKeys.filter(key => {
-            if (parseInt(key, 10) >= nextLength) {
-              currentOperation.push({
-                path: collections.concat(key),
-                isDelete: true,
-              });
-              return true;
-            }
-            return false;
-          });
-        }
-      }
-
-      if (isObject(nextValue) && isObject(baseValue)) {
-        const nextKeys = Object.keys(nextValue);
-        const prevKeys = Object.keys(baseValue);
-        const removed = diffArraySimple(prevKeys, nextKeys);
-
-        if (removed.length) {
-          toDestroy.push(
-            ((branch: PathNode, removed: Array<string>) => {
-              removed.forEach(key => {
-                const childBranch = branch.children[key];
-                if (childBranch) childBranch.destroyPathNode();
-              });
-            }).bind(null, branch, removed)
-          );
-        }
-      }
-
-      if (this.strictMode) {
-        keysToCompare.forEach(key => {
-          const childBranch = branch.children[key];
-          if (!baseValue || typeof baseValue[key] === 'undefined') {
-            childBranch.patchers.forEach(patcher => {
-              const displayName = patcher.displayName;
-              const joinedPath = collections.concat(key).join('.');
-              console.warn('root base value ', rootBaseValue) // eslint-disable-line
-              console.warn( // eslint-disable-line
-                `Maybe you are using an un-declared props %c${joinedPath}` +
-                  ` %cin Component %c${displayName} %cYou'd better declare this prop in model first,` +
-                  'or component may not re-render when value changes on ES5.',
-                'color: #ff4d4f; font-weight: bold',
-                '',
-                'color: #7cb305; font-weight: bold',
-                ''
-              );
-            });
+        if (isTypeEqual(oldValue, newValue)) {
+          if (isMutable(newValue)) {
+            const childBranch = branch.children[key];
+            compare(childBranch, oldValue, newValue, [], []);
+            return;
           }
-        });
-      }
 
-      keysToCompare.forEach(key => {
-        const childBranch = branch.children[key];
-        const childBaseValue = baseValue ? baseValue[key] : undefined;
-        // 当一个对象中的key被删除的时候，那么它的值就是undefined
-        const childNextValue = nextValue ? nextValue[key] : undefined;
+          if (isNumber(newValue) || isString(newValue)) {
+            if (oldValue !== newValue) {
+              if (branch.children[key].patchers.length) {
+                branch.children[key].patchers.forEach(patcher => {
+                  patchers.push({ collections, patcher, operation });
+                });
+                // delete should be placed after collection...
+                // `branch.patchers` will be modified on `markDirty`..
+                // branch.patchers.forEach(patcher => patcher.markDirtyAll())
+                // branch.patchers.forEach(patcher => patcher.markDirty());
+              }
+            }
+          }
 
-        compare(
-          childBranch,
-          childBaseValue,
-          childNextValue,
-          collections.concat(key),
-          currentOperation
-        );
+          return;
+        }
+
+        // if (branch.patchers.length) {
+        //   branch.patchers.forEach(patcher => {
+        //     patchers.push({ collections, patcher, operation });
+        //   });
+        //   // delete should be placed after collection...
+        //   // `branch.patchers` will be modified on `markDirty`..
+        //   // branch.patchers.forEach(patcher => patcher.markDirtyAll())
+        //   branch.patchers.forEach(patcher => patcher.markDirty());
+        // }
       });
 
-      if (keysToDestroy.length) {
-        toDestroy.push(
-          ((branch: PathNode, keysToDestroy: Array<string>) => {
-            keysToDestroy.forEach(key => {
-              const childBranch = branch.children[key];
-              if (childBranch) childBranch.destroyPathNode();
-            });
-          }).bind(null, branch, keysToDestroy)
-        );
-      }
+      // if (is(baseValue, nextValue)) return;
+
+      // // TODO, add description...only primitive type react...
+      // if (!isTypeEqual(baseValue, nextValue) || !isMutable(nextValue)) {
+      //   if (branch.patchers.length) {
+      //     branch.patchers.forEach(patcher => {
+      //       this.pendingPatchers.push({ collections, patcher, operation });
+      //     });
+      //     // delete should be placed after collection...
+      //     // `branch.patchers` will be modified on `markDirty`..
+      //     // branch.patchers.forEach(patcher => patcher.markDirtyAll())
+      //     branch.patchers.forEach(patcher => patcher.markDirty());
+      //   }
+      // }
+
+      // const caredKeys = Object.keys(branch.children);
+      // let keysToCompare = caredKeys;
+      // let keysToDestroy: Array<string> = [];
+      // const currentOperation: Array<Operation> = [];
+
+      // // 处理，如果说array中的一项被删除了。。。。
+      // if (isTypeEqual(baseValue, nextValue) && Array.isArray(nextValue)) {
+      //   const baseLength = (baseValue as any).length;
+      //   const nextLength = nextValue.length;
+
+      //   if (nextLength < baseLength) {
+      //     keysToCompare = caredKeys.filter(
+      //       key => parseInt(key, 10) < nextLength || key === 'length'
+      //     );
+      //     keysToDestroy = caredKeys.filter(key => {
+      //       if (parseInt(key, 10) >= nextLength) {
+      //         currentOperation.push({
+      //           path: collections.concat(key),
+      //           isDelete: true,
+      //         });
+      //         return true;
+      //       }
+      //       return false;
+      //     });
+      //   }
+      // }
+
+      // if (isObject(nextValue) && isObject(baseValue)) {
+      //   const nextKeys = Object.keys(nextValue);
+      //   const prevKeys = Object.keys(baseValue);
+      //   const removed = diffArraySimple(prevKeys, nextKeys);
+
+      //   if (removed.length) {
+      //     toDestroy.push(
+      //       ((branch: PathNode, removed: Array<string>) => {
+      //         removed.forEach(key => {
+      //           const childBranch = branch.children[key];
+      //           if (childBranch) childBranch.destroyPathNode();
+      //         });
+      //       }).bind(null, branch, removed)
+      //     );
+      //   }
+      // }
+
+      // if (this.strictMode) {
+      //   keysToCompare.forEach(key => {
+      //     const childBranch = branch.children[key];
+      //     if (!baseValue || typeof baseValue[key] === 'undefined') {
+      //       childBranch.patchers.forEach(patcher => {
+      //         const displayName = patcher.displayName;
+      //         const joinedPath = collections.concat(key).join('.');
+      //         console.warn('root base value ', rootBaseValue) // eslint-disable-line
+      //         console.warn( // eslint-disable-line
+      //           `Maybe you are using an un-declared props %c${joinedPath}` +
+      //             ` %cin Component %c${displayName} %cYou'd better declare this prop in model first,` +
+      //             'or component may not re-render when value changes on ES5.',
+      //           'color: #ff4d4f; font-weight: bold',
+      //           '',
+      //           'color: #7cb305; font-weight: bold',
+      //           ''
+      //         );
+      //       });
+      //     }
+      //   });
+      // }
+
+      // keysToCompare.forEach(key => {
+      //   const childBranch = branch.children[key];
+      //   const childBaseValue = baseValue ? baseValue[key] : undefined;
+      //   // 当一个对象中的key被删除的时候，那么它的值就是undefined
+      //   const childNextValue = nextValue ? nextValue[key] : undefined;
+
+      //   compare(
+      //     childBranch,
+      //     childBaseValue,
+      //     childNextValue,
+      //     collections.concat(key),
+      //     currentOperation
+      //   );
+      // });
+
+      // if (keysToDestroy.length) {
+      //   toDestroy.push(
+      //     ((branch: PathNode, keysToDestroy: Array<string>) => {
+      //       keysToDestroy.forEach(key => {
+      //         const childBranch = branch.children[key];
+      //         if (childBranch) childBranch.destroyPathNode();
+      //       });
+      //     }).bind(null, branch, keysToDestroy)
+      //   );
+      // }
     };
 
     compare(branch, baseValue, nextValue, [storeKey as any], []);
@@ -255,13 +301,6 @@ class Application<T, K extends keyof T> implements IApplication<T, K> {
 
   getStoreData(storeName: K): T[K] {
     const storeValue = this.base[storeName];
-
-    // on iOS 10. toString(new Proxy({}, {}) === 'object ProxyObject')
-    // invariant(
-    //   !!storeValue,
-    //   `Invalid storeName '${storeName}'.` +
-    //     'Please ensure `base[storeName]` return non-undefined value '
-    // );
 
     return storeValue;
   }
--- src/utils/ifType.ts
+++ src/utils/ifType.ts
@@ -3,6 +3,8 @@ import toString from './toString';
 export const isPresent = (o: any) => typeof o !== 'undefined';
 export const isObject = (obj: any) => toString(obj) === '[object Object]';
 export const isArray = (obj: any) => toString(obj) === '[object Array]';
+export const isNumber = (obj: any) => toString(obj) === '[object Number]';
+export const isString = (obj: any) => toString(obj) === '[object String]';
 
 export const isMutable = (obj: any) => isObject(obj) || isArray(obj);
 export const isTypeEqual = (o1: any, o2: any) => toString(o1) === toString(o2);
